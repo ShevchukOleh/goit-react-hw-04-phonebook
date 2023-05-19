34f35499c0018f340a6e8ac3e69af5e71f778722
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-05-19 19:21:38 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,21 @@ import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
+  const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ?? 
+  [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ])
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const storedContacts = JSON.parse(localStorage.getItem('contacts'));
-
-    if (storedContacts) {
-      setContacts(storedContacts);
+  useEffect((prevState) =>{
+    if(prevState !== contacts) {
+      const localString = JSON.stringify(contacts)
+      window.localStorage.setItem('contacts', localString)
     }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  }, [contacts])
 
   const handleSubmit = (formData) => {
     const { name } = formData;
@@ -45,7 +41,6 @@ const App = () => {
     const { value } = e.target;
     setFilter(value);
   };
-
 
   const visibleContacts = () => {
     return contacts.filter((contact) =>
